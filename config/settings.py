ad7cb500e4960e392d61db355e5dc8c1584fd3f1
codeercpp-byte <codeercpp@gmail.com>
2026-02-24 19:32:50 +0500

    Remove db from repo

--- config/settings.py
+++ config/settings.py
@@ -15,7 +15,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
-SITE_URL = "http://127.0.0.1:8000"
+SITE_URL = "https://oqdaryommtb.uz"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
