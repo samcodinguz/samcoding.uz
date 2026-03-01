3e3535fd5d15fb92f7827e0f8a9ce4a866ae4bdb
codeercpp-byte <codeercpp@gmail.com>
2026-03-01 20:16:52 +0500

    Remove db from repo

--- config/errors.py
+++ config/errors.py
@@ -1,22 +1,22 @@
 from django.shortcuts import render
 
 def error_400(request, exception=None):
-    return render(request, 'core/errors/400.html', status=400)
+    return render(request, 'core/public/errors/400.html', status=400)
 
 def error_401(request, exception=None):
-    return render(request, 'core/errors/401.html', status=401)
+    return render(request, 'core/public/errors/401.html', status=401)
 
 def error_402(request, exception=None):
-    return render(request, 'core/errors/402.html', status=402)
+    return render(request, 'core/public/errors/402.html', status=402)
 
 def error_403(request, exception=None):
-    return render(request, 'core/errors/403.html', status=403)
+    return render(request, 'core/public/errors/403.html', status=403)
 
 def error_404(request, exception=None):
-    return render(request, 'core/errors/404.html', status=404)
+    return render(request, 'core/public/errors/404.html', status=404)
 
 def error_408(request, exception=None):
-    return render(request, 'core/errors/408.html', status=408)
+    return render(request, 'core/public/errors/408.html', status=408)
 
 def error_500(request):
-    return render(request, 'core/errors/500.html', status=500)
+    return render(request, 'core/public/errors/500.html', status=500)
